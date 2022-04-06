085280e7162f43960021ca35dd06782b3ab72eaa
Loscho <loscho@pop-os.localdomain>
2022-04-06 05:14:45 -0500

    Modified and readded back missing files.

--- IS310_Assignments/dh_tools.py
+++ IS310_Assignments/dh_tools.py
@@ -14,18 +14,18 @@ with open("tools_dh_proceedings.csv","r") as input_file:
         print("Tool:" + row['Tool'], 
         "\n2015: " + row['2015'], 
         "\n2019: " + row['2019'],
-        "\nOverall Count: " + str(overall_count) + "\n")
+        "\nOverall Count:", overall_count, "\n")
         
 # Step 4
     def make_dh_tool(name, v2015, v2016, v2017, v2018, v2019):
         count = (v2015, v2016, v2017, v2018, v2019)
         overall_count = sum(count)
         print("Tool:" + name, 
-        "\n2015: " + str(v2015),
-        "\n2016: " + str(v2016), 
-        "\n2017: " + str(v2017), 
-        "\n2018: " + str(v2018),  
-        "\n2019: " + str(v2019),
-        "\nOverall Count: " + str(overall_count) + "\n")
+        "\n2015:", v2015,
+        "\n2016:", v2016, 
+        "\n2017:", v2017, 
+        "\n2018:", v2018,  
+        "\n2019:", v2019,
+        "\nOverall Count:", overall_count, "\n")
 
     make_dh_tool("New Tool", 5, 1, 4, 3, 2)
---
+++ IS310_Assignments/nakagawa_first_python_assignment.py
@@ -0,0 +1,128 @@
+tools = {
+   "tool_1": {
+       'name': 'Python',
+       '2015': 9,
+       '2016': 22,
+       '2017': 27,
+       '2018': 32,
+       '2019': 35,
+       'Total': 125
+   },
+   "tool_2": {
+       'name': 'JavaScript',
+       '2015': 8,
+       '2016': 18,
+       '2017': 12,
+       '2018': 6,
+       '2019': 15,
+       'Total': 59
+   },
+   "tool_3": {
+       'name': 'Twitter',
+       '2015': 10,
+       '2016': 18,
+       '2017': 26,
+       '2018': 16,
+       '2019': 12,
+       'Total': 82
+   },
+    "tool_4": {
+       'name': 'Github',
+       '2015': 2,
+       '2016': 6,
+       '2017': 17,
+       '2018': 5,
+       '2019': 10,
+       'Total': 40
+    },
+    "tool_5": {
+       'name': 'Gephi',
+       '2015': 11,
+       '2016': 16,
+       '2017': 14,
+       '2018': 10,
+       '2019': 9,
+       'Total': 60
+    },
+    "tool_6": {
+       'name': 'GeoNames',
+       '2015': 2,
+       '2016': 4,
+       '2017': 3,
+       '2018': 1,
+       '2019': 8,
+       'Total': 18
+    },
+    "tool_7": {
+       'name': 'Transkribus',
+       '2015': 0,
+       '2016': 1,
+       '2017': 2,
+       '2018': 1,
+       '2019': 8,
+       'Total': 12
+    },
+      "tool_8": {
+       'name': 'Excel',
+       '2015': 5,
+       '2016': 9,
+       '2017': 3,
+       '2018': 6,
+       '2019': 7,
+       'Total': 30
+    },
+      "tool_9": {
+       'name': 'MySQL',
+       '2015': 0,
+       '2016': 6,
+       '2017': 9,
+       '2018': 5,
+       '2019': 7,
+       'Total': 27
+    }
+}
+
+print(tools['tool_1']['name'],
+'\n2015:', tools['tool_1']['2015'],
+'\n2019:', tools['tool_1']['2019'],
+'\nTotal:', tools['tool_1']['Total'], "\n")
+
+print(tools['tool_2']['name'],
+'\n2015:', tools['tool_2']['2015'],
+'\n2019:', tools['tool_2']['2019'],
+'\nTotal:', tools['tool_2']['Total'], "\n")
+
+print(tools['tool_3']['name'],
+'\n2015:', tools['tool_3']['2015'],
+'\n2019:', tools['tool_3']['2019'],
+'\nTotal:', tools['tool_3']['Total'], "\n")
+
+print(tools['tool_4']['name'],
+'\n2015:', tools['tool_4']['2015'],
+'\n2019:', tools['tool_4']['2019'],
+'\nTotal:', tools['tool_4']['Total'], "\n")
+
+print(tools['tool_5']['name'],
+'\n2015:', tools['tool_5']['2015'],
+'\n2019:', tools['tool_5']['2019'],
+'\nTotal:', tools['tool_5']['Total'], "\n")
+
+print(tools['tool_6']['name'],
+'\n2015:', tools['tool_6']['2015'],
+'\n2019:', tools['tool_6']['2019'],
+'\nTotal:', tools['tool_6']['Total'], "\n")
+
+print(tools['tool_7']['name'],
+'\n2015:', tools['tool_7']['2015'],
+'\n2019:', tools['tool_7']['2019'],
+'\nTotal:', tools['tool_7']['Total'], "\n")
+
+print(tools['tool_8']['name'],
+'\n2015:', tools['tool_8']['2015'],
+'\n2019:', tools['tool_8']['2019'],
+'\nTotal:', tools['tool_8']['Total'], "\n")
+
+print(tools['tool_9']['name'],
+'\n2015:', tools['tool_9']['2015'],
+'\n2019:', tools['tool_9']['2019'],
+'\nTotal:', tools['tool_9']['Total']), "\n"
